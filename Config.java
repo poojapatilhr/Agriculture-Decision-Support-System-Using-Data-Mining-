682540601a0a4e572f89165d392db739ad3e0ff3
poojapatilhr <68379825+poojapatilhr@users.noreply.github.com>
2020-07-16 06:53:06 -0500

    Create Config.java

---
+++ Config.java
@@ -0,0 +1,10 @@
+package com.cropprediction;
+
+
+public class Config {
+
+    public static final String SP_FILE_NAME = "Crop";
+    //public static final String JSON_URL = "http://192.168.1.11/crop_prediction/";
+    public static final String JSON_URL = "http://cropprediction.000webhostapp.com/webservices/";
+    public static final String PDF_URL = "https://cropprediction.000webhostapp.com/pdf/";
+}
